7ce47c02b12c1b6097b1efcf0dfd7934a6a911d2
Maxwell Kretschmer <maxwellkret@gmail.com>
2021-11-23 04:04:21 -0600

    FINALLY

--- Server/FlaskEx/fileserver.py
+++ Server/FlaskEx/fileserver.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, request
-
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def form():
-    return render_template('form.html')
-
-@app.route('/hello', methods=['GET', 'POST'])
-def hello():
-    return render_template('greeting.html', say=request.form['say'], to=request.form['to'])
+@app.route('/')
+def upload_page():
+   return render_template('upload.html')
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
